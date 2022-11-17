b893db1227f738e2452683a4234a84114d85db4f
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-17 20:28:32 +0300

    feat: Integer_arithmetic 2

--- Integer_arithmetic/2/index.php
+++ Integer_arithmetic/2/index.php
@@ -1,1 +1,30 @@
 <?php
+
+function ifSequence(int $n): bool
+{
+  $rest = $n;
+  $res = false;
+
+  $rest = $rest / 10;
+  $prevFract = (int)(($rest - floor($rest)) * 10);
+
+  while ($rest > 1) {
+
+    $rest = $rest / 10;
+    $fract = (int)(($rest - floor($rest)) * 10);
+
+    if ($prevFract > $fract) {
+      $res = true;
+    } else {
+
+      return false;
+    }
+
+    $prevFract = $fract;
+  }
+
+  return $res;
+}
+
+var_dump(ifSequence(1623456789));
+var_dump(ifSequence(123456789));
