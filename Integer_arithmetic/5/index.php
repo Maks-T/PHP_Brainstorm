0eb9cb1c49fdd8462f99966af8c4121e8acfca4a
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-18 00:16:49 +0300

    feat: Integer_arithmetic 5

--- Integer_arithmetic/5/index.php
+++ Integer_arithmetic/5/index.php
@@ -1,1 +1,27 @@
 <?php
+
+function isSymmetrical(int $n): bool
+{
+  $length = (int) log10($n) + 1;
+  if ($length % 2 !== 0) {
+    return false;
+  }
+
+  $right = $n % 10 ** ($length / 2);
+  $left = (int)($n / 10 ** ($length / 2));
+
+  for ($i = 0; $i < $length / 2; $i++) {
+    $dr = (int)($right / 10 ** $i) % 10;
+    $dl = (int)($left / 10 ** ($length / 2 - $i - 1)) % 10;
+
+    if ($dr !== $dl) {
+
+      return false;
+    }
+  }
+
+  return true;
+}
+
+var_dump(isSymmetrical(1412332141));
+var_dump(isSymmetrical(14123321417));
