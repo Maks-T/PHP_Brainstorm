23f62e5103ab141eef801a48ca3060f7ba7d9eb2
Прораб <@SP.int>
2022-11-18 11:18:41 +0300

    feat: One-dimensional_arrays 2

--- One-dimensional_arrays/2/index.php
+++ One-dimensional_arrays/2/index.php
@@ -5,3 +5,20 @@
  * которого равен среднему арифметическому
  * первых i элементов массива А.
  */
+
+ function getAverageArr(array $arr) 
+ {
+  $l = count($arr);
+  $curSum = 0;
+  $res = [];
+
+  for ($i = 0; $i<$l; $i++) {
+    $curSum += $arr[$i];
+    $res[] = $curSum/($i+1);
+  }
+
+  return $res;
+ }
+
+ var_dump(getAverageArr([1,2,3]));
+ var_dump(getAverageArr([6,6,6,6,6,6,1]));
