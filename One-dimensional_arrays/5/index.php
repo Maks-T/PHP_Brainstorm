c9d6ad4bf2d9612d43cc96b8ec15dcac5a97cb61
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-18 17:53:53 +0300

    feat: One-dimensional_arrays 5

--- One-dimensional_arrays/5/index.php
+++ One-dimensional_arrays/5/index.php
@@ -6,3 +6,27 @@
  * расположились все нули, затем все двойки и, 
  * наконец, все единицы.
  */
+
+function getSortArray(array $arr): array
+{
+  $l = count($arr);
+  $zeroArr = [];
+  $oneArr = [];
+  $twoArr = [];
+
+  for ($i = 0; $i < $l; $i++) {
+    if ($arr[$i] === 0) {
+      $zeroArr[] = 0;
+    } elseif ($arr[$i] === 1) {
+      $oneArr[] = 1;
+    } elseif ($arr[$i] === 2) {
+      $twoArr[] = 2;
+    }
+  }
+
+  return array_merge($zeroArr, $oneArr, $twoArr);
+}
+
+echo '<pre>';
+print_r(getSortArray([0, 2, 1, 0, 2, 1, 0, 2, 1, 1, 0, 2, 1, 0, 2, 1, 1, 0, 2, 0]));
+echo '</pre>';
