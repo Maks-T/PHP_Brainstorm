5976901ad895fc6b814c4a604dd6ea9247c4c383
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-18 19:30:06 +0300

    refactor: Integer_arithmetic

--- Integer_arithmetic/6/index.php
+++ Integer_arithmetic/6/index.php
@@ -1,16 +1,21 @@
 <?php
 
-function gcd(int $a, int $b): int 
+/**  Найти наибольший общий делитель (НОД) 
+ * двух натуральных чисел N и M, 
+ * реализуя алгоритм эвклида. 
+ */
+
+function gcd(int $a, int $b): int
 {
-  while ($a !== 0 && $b !==0) {
-    if ($a>$b) {
+  while ($a !== 0 && $b !== 0) {
+    if ($a > $b) {
       $a = $a % $b;
     } else {
       $b = $b % $a;
     }
   }
 
-  return $a+$b;
+  return $a + $b;
 }
 
 echo gcd(20, 5);
