dee5e5d5e249a9a27239b1c0b338b1fa54894ecc
Прораб <@SP.int>
2022-11-18 12:09:36 +0300

    feat: One-dimensional_arrays 3

--- One-dimensional_arrays/3/index.php
+++ One-dimensional_arrays/3/index.php
@@ -4,4 +4,30 @@
  * В массиве А(N) подсчитать количество различных элементов.
  */
 
- 
+ function countDifferentElem(array $arr) 
+ {
+  $res = [];
+  $li = count($arr);
+
+  for ($i = 0; $i<$li; $i++) {
+
+    $lj = count($res);
+    $isExist = true;
+
+    for ($j = 0; $j<$lj; $j++) {
+      if ($arr[$i] === $res[$j]) {
+        $isExist = false;
+        echo  $res[$j] . '<br>';
+      }
+    }
+
+    if ($isExist) {
+      $res[] = $arr[$i];
+    }
+  }
+
+  return count($res);
+}
+
+var_dump(countDifferentElem([1,2,2,5,5,6,6,1,1,2,2,8]));
+
