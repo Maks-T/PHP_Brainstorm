4f684ef36fe8d53951f30c1c1a4836996a2caf61
Прораб <@SP.int>
2022-11-18 08:08:23 +0300

    feat: Integer_arithmetic 6

--- Integer_arithmetic/6/index.php
+++ Integer_arithmetic/6/index.php
@@ -1,1 +1,16 @@
 <?php
+
+function gcd(int $a, int $b): int 
+{
+  while ($a !== 0 && $b !==0) {
+    if ($a>$b) {
+      $a = $a % $b;
+    } else {
+      $b = $b % $a;
+    }
+  }
+
+  return $a+$b;
+}
+
+echo gcd(20, 5);
