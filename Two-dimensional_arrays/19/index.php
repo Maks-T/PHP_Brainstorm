b50b27bed488225f4e4dd2e74489972ef0472487
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 22:08:14 +0300

    feat: One-dimensional_arrays 19

--- Two-dimensional_arrays/19/index.php
+++ Two-dimensional_arrays/19/index.php
@@ -5,9 +5,41 @@
  * заданное число р, не нарушая этот порядок.
  * */
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function trasformMatrix(array $arr, int $p): array
 {
-  
-  return 
+  $li = count($arr);
+
+  for ($i = 0; $i < $li; $i++) {
+    $arr[$i][] = $p;
+
+    $arr[$i] = sortBubbleDown($arr[$i]);
+  }
+
+  return $arr;
 }
 
+function sortBubbleDown(array $arr): array
+{
+  $l = count($arr);
+
+  for ($i = 0; $i < $l; $i++) {
+    for ($j = 1; $j < $l - $i; $j++) {
+      if ($arr[$j] > $arr[$j - 1]) {
+        [$arr[$j], $arr[$j - 1]] = [$arr[$j - 1], $arr[$j]];
+      }
+    }
+  }
+
+  return $arr;
+}
+
+echo '<pre>';
+print_r(
+  trasformMatrix([
+    [101, 11, 2, 100, 4, 8, 10],
+    [1, 2, 70, 14, 16, 20],
+    [1, 2, 70, 14, 11, 21, 30],
+  ], 15)
+);
+echo '</pre>';
