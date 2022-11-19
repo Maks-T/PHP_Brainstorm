4d2ebba4f856ef02d681bb88ec48ebe6867f7b8c
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 20:18:47 +0300

    feat: One-dimensional_arrays 15

--- Two-dimensional_arrays/15/index.php
+++ Two-dimensional_arrays/15/index.php
@@ -4,9 +4,63 @@
  * нечетных элементов переставить на последнее место.
  */
 
-function f(array $arr ) 
+/** @var array[] $arr  */
+function trasformMatrix(array $arr): array
 {
-  
-  return 
+  $arr = revertMatrix($arr);
+  $l = count($arr);
+  $imin = 0;
+
+  for ($i = 1; $i < $l; $i++) {
+    if (countOdd($arr[$i]) < countOdd($arr[$i - 1])) {
+      $imin = $i;
+    }
+  }
+
+  $alterArr = $arr[$imin];
+
+  array_splice($arr, $imin, 1);
+  $arr[] = $alterArr;
+
+  return revertMatrix($arr);
 }
 
+function countOdd(array $arr): int
+{
+  $l = count($arr);
+  $sumOdd = 0;
+
+  for ($i = 0; $i < $l; $i++) {
+    if ($arr[$i] % 2 !== 0) {
+      $sumOdd++;
+    }
+  }
+
+  return $sumOdd;
+}
+
+/** @var array[] $arr */
+function revertMatrix($arr): array
+{
+  $li = count($arr);
+  $lj = count($arr[0]);
+  $res = [];
+  for ($j = 0; $j < $lj; $j++) {
+    $column = [];
+    for ($i = 0; $i < $li; $i++) {
+      $column[] = $arr[$i][$j];
+    }
+    $res[] = $column;
+  }
+  return $res;
+}
+
+echo '<pre>';
+print_r(
+  trasformMatrix([
+    [1, 3, 3, 7, 9],
+    [1, 3, 2, 7, 9],
+    [1, 3, 2, 7, 9],
+  ])
+);
+echo '</pre>';
