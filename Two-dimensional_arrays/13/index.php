c22d291a5ea9974b84a2cf8465fac12de1dc443d
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 19:54:53 +0300

    feat: One-dimensional_arrays 13

--- Two-dimensional_arrays/13/index.php
+++ Two-dimensional_arrays/13/index.php
@@ -4,11 +4,58 @@
  *  его перед последним столбцом, содержащим нулевой элемент. 
  * Если такого столбца не окажется, то вставить массив
  * В(N) после последнего столбца.
-*/
+ */
 
-function f(array $arr ) 
+/** @var array[] $arr  */
+function trasformMatrix(array $arr, array $add_arr): array
 {
-  
-  return 
+  $arr = revertMatrix($arr);
+
+  $li = count($arr);
+  $lj = count($arr[0]);
+  $iz = -1;
+
+  for ($i = $li - 1; $i >= 0; $i--) {
+    for ($j = $lj - 1; $j >= 0; $j--) {
+      if ($arr[$i][$j] === 0) {
+        $iz = $i;
+        break 2;
+      }
+    }
+  }
+
+  if ($iz === -1) {
+    $arr[] = $add_arr;
+  } else {
+    array_splice($arr, $iz, 0, [$add_arr]);
+  }
+
+  return revertMatrix($arr);
 }
 
+
+/** @var array[] $arr */
+function revertMatrix($arr): array
+{
+  $li = count($arr);
+  $lj = count($arr[0]);
+  $res = [];
+  for ($j = 0; $j < $lj; $j++) {
+    $column = [];
+    for ($i = 0; $i < $li; $i++) {
+      $column[] = $arr[$i][$j];
+    }
+    $res[] = $column;
+  }
+  return $res;
+}
+
+echo '<pre>';
+print_r(
+  trasformMatrix([
+    [1, 2, 0, 1, 7],
+    [1, 4, 3, 1, 1],
+    [1, 0, 3, 5, 7],
+  ], [-2, -3, -4])
+);
+echo '</pre>';
