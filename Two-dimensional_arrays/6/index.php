2ee0cfaf6bd12a25c5163539231e9b1a5c36ca26
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 12:10:38 +0300

    feat: One-dimensional_arrays 6

--- Two-dimensional_arrays/6/index.php
+++ Two-dimensional_arrays/6/index.php
@@ -4,9 +4,48 @@
  *  отрицательных элементов, и умножить ее как вектор на матрицу А.
  */
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function multiplyMatrix(array $arr): array
 {
-  
-  return 
+  $li = count($arr);
+  $vector = [];
+
+  for ($i = 0; $i < $li; $i++) {
+    if (isNotNegative($arr[$i])) {
+      $vector = $arr[$i];
+      break;
+    }
+  }
+
+  for ($i = 0; $i < $li; $i++) {
+    $lj = count($arr[$i]);
+    for ($j = 0; $j < $lj; $j++) {
+      $arr[$i][$j] *= $vector[$i];
+    }
+  }
+
+  return $arr;
 }
 
+function isNotNegative(array $arr): bool
+{
+  $l = count($arr);
+
+  for ($i = 0; $i < $l; $i++) {
+    if ($arr[$i] < 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+echo '<pre>';
+print_r(
+  multiplyMatrix([
+    [1, -2, 3],
+    [1, -1, 1],
+    [2, 3, 1],
+  ])
+);
+echo '</pre>';
