8f2664e055078860939649192f673f41c1708b17
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 21:18:57 +0300

    feat: One-dimensional_arrays 17

--- Two-dimensional_arrays/17/index.php
+++ Two-dimensional_arrays/17/index.php
@@ -3,9 +3,50 @@
 /**  В массиве А(N,M) элементы, кратные заданному числу р, 
  * переместить в начало строк и расположить их в порядке возрастания.*/
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function trasformMatrix(array $arr, int $p): array
 {
-  
-  return 
+  $li = count($arr);
+
+  for ($i = 0; $i < $li; $i++) {
+    $lj = count($arr[$i]);
+    $multiple = [];
+    $rest = [];
+
+    for ($j = 0; $j < $lj; $j++) {
+      if ($arr[$i][$j] % $p === 0) {
+        $multiple[] = $arr[$i][$j];
+      } else {
+        $rest[] = $arr[$i][$j];
+      }
+    }
+
+    $arr[$i] = [...sortBubble($multiple), ...$rest];
+  }
+
+  return $arr;
 }
 
+function sortBubble(array $arr): array
+{
+  $l = count($arr);
+
+  for ($i = 0; $i < $l; $i++) {
+    for ($j = 1; $j < $l - $i; $j++) {
+      if ($arr[$j] < $arr[$j - 1]) {
+        [$arr[$j], $arr[$j - 1]] = [$arr[$j - 1], $arr[$j]];
+      }
+    }
+  }
+
+  return $arr;
+}
+
+echo '<pre>';
+print_r(
+  trasformMatrix([
+    [101, 11, 2, 100, 4, 8, 10],
+    [1, 2, 70, 14, 16, 20],
+  ], 2)
+);
+echo '</pre>';
