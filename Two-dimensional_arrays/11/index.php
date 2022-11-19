85f811250f20a3e6599a579ad3eb29401eb0d1df
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 19:29:31 +0300

    feat: One-dimensional_arrays 11

--- Two-dimensional_arrays/11/index.php
+++ Two-dimensional_arrays/11/index.php
@@ -2,9 +2,37 @@
 
 /**  В массиве А(N,М) удалить нулевые строки. */
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function trasformMatrix(array $arr): array
 {
-  
-  return 
+  for ($i = 0; $i < count($arr); $i++) {
+    $lj = count($arr[$i]);
+    $isZeroLine = true;
+
+    for ($j = 0; $j < $lj; $j++) {
+      if ($arr[$i][$j] !== 0) {
+        $isZeroLine = false;
+        break;
+      }
+    }
+
+    if ($isZeroLine) {
+      array_splice($arr, $i, 1);
+    }
+  }
+
+  return $arr;
 }
 
+echo '<pre>';
+print_r(
+  trasformMatrix([
+    [4, 0, -1],
+    [4, 4, 5],
+    [0, 0, 0],
+    [4, 0, -1],
+    [0, 0, 0],
+    [4, 6, 10],
+  ])
+);
+echo '</pre>';
