08308b7adebcd1035604d0da7fc173f23f7b22d0
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 19:17:07 +0300

    feat: One-dimensional_arrays 10

--- Two-dimensional_arrays/10/index.php
+++ Two-dimensional_arrays/10/index.php
@@ -4,9 +4,55 @@
  * содержащие максимальный и минимальный элементы.
  */
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function trasformMatrix(array $arr): array
 {
-  
-  return 
+  $li = count($arr);
+  $lj = count($arr[0]);
+  $min = null;
+  $max = null;
+  $jmin = 0;
+  $jmax = 0;
+
+  for ($j = 0; $j < $lj; $j++) {
+    //get min and max element
+    for ($i = 0; $i < $li; $i++) {
+      if ($min !== null && $arr[$i][$j] < $min) {
+        $min = $arr[$i][$j];
+        $jmin = $j;
+      }
+
+      if ($min === null) {
+        $min = $arr[$i][$j];
+        $jmin = $j;
+      }
+
+      if ($max !== null && $arr[$i][$j] > $max) {
+        $max = $arr[$i][$j];
+        $jmax = $j;
+      }
+
+      if ($max === null) {
+        $max = $arr[$i][$j];
+        $jmax = $j;
+      }
+    }
+  }
+
+  //change columns
+  for ($i = 0; $i < $li; $i++) {
+    [$arr[$i][$jmax], $arr[$i][$jmin]] = [$arr[$i][$jmin], $arr[$i][$jmax]];
+  }
+
+  return $arr;
 }
 
+echo '<pre>';
+print_r(
+  trasformMatrix([
+    [4, 0, -1, 3],
+    [4, 4, 5, 6],
+    [4, 6, 10, 100],
+  ])
+);
+echo '</pre>';
