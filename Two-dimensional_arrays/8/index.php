ba509d60908440c64145a9ebb6bfc439475f66b7
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 18:44:53 +0300

    feat: One-dimensional_arrays 8

--- Two-dimensional_arrays/8/index.php
+++ Two-dimensional_arrays/8/index.php
@@ -5,9 +5,55 @@
  * и последним максимальными элементами.
  */
 
-function f(array $arr ) 
+function transformArr(array $arr)
 {
-  
-  return 
+  $li = count($arr);
+
+  for ($i = 0; $i < $li; $i++) {
+
+    [$jf, $jl] =  getTwoMaxIndex($arr[$i]);
+    $lj = count($arr[$i]);
+    $transArr = [];
+
+    for ($j = 0; $j < $lj; $j++) {
+      if ($j <= $jf || $j >= $jl) {
+        $transArr[] = $arr[$i][$j];
+      } elseif ($arr[$i][$j] < 0 || $arr[$i][$j] % 2 !== 0) {
+        $transArr[] = $arr[$i][$j];
+      }
+    }
+    $arr[$i] = $transArr;
+  }
+
+  return $arr;
 }
 
+function getTwoMaxIndex(array $arr)
+{
+  $l = count($arr);
+  $if = 0;
+  $il = 0;
+
+  for ($i = 1; $i < $l; $i++) {
+    if ($arr[$i] > $arr[$il]) {
+      $il = $i;
+    }
+
+    if (!$if) {
+      $if = $il;
+    }
+  }
+
+  return [$if, $il];
+}
+
+
+echo '<pre>';
+print_r(
+  transformArr([
+    [1, -1, 2, 3, 4, -1, 5, 6, 1, 2, 8, -10, 1, 2],
+    [4, 5, -4, 3, 2, 6, -1],
+
+  ])
+);
+echo '</pre>';
