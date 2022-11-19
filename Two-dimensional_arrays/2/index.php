a0d575e4532ec71dcf65f2b161c4ab149801f7ba
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 11:32:44 +0300

    feat: One-dimensional_arrays 2

--- Two-dimensional_arrays/2/index.php
+++ Two-dimensional_arrays/2/index.php
@@ -5,9 +5,46 @@
  * которой является наименьшим. 
  */
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function getSums(array $arr): ?int
 {
-  
-  return 
+  $minAvg = null;
+  $nLine = null;
+
+  $l = count($arr);
+
+  for ($i = 0; $i < $l; $i++) {
+    $countPos = 0;
+    $sumPos = 0;
+
+    for ($j = 0; $j < $l; $j++) {
+      if ($arr[$i][$j] > 0) {
+        $countPos += 1;
+        $sumPos += $arr[$i][$j];
+      }
+    }
+
+    if ($minAvg) {
+      if ($sumPos / $countPos < $minAvg) {
+        $minAvg = $sumPos / $countPos;
+        $nLine = $i;
+      }
+    } else {
+      $minAvg = $sumPos / $countPos;
+      $nLine = $i;
+    }
+  }
+
+  return $nLine;
 }
 
+echo '<pre>';
+print_r(
+  getSums([
+    [100, 2, 3, 4],
+    [5, 6, 7, 8],
+    [-1009, -10, -11, 3],
+    [0, 0, 0, 1]
+  ])
+);
+echo '</pre>';
