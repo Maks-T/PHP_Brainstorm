1694b3cfebcb3beba536b40e4cd148f0376f00b3
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 12:00:54 +0300

    feat: One-dimensional_arrays 5

--- Two-dimensional_arrays/5/index.php
+++ Two-dimensional_arrays/5/index.php
@@ -3,9 +3,32 @@
 /** В массиве А(N,М) найти сумму элементов тех столбцов, 
  * все элементы которых кратны заданному числу р .*/
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function sumColumn(array $arr, int $n)
 {
-  
-  return 
+  $sum = 0;
+  $l = count($arr);
+
+  for ($j = 0; $j < $l; $j++) {
+    $sumColumn = 0;
+    for ($i = 0; $i < $l; $i++) {
+      if ($arr[$i][$j] % $n === 0) {
+        $sumColumn += $arr[$i][$j];
+      } else {
+        $sumColumn = 0;
+        break;
+      }
+    }
+    $sum += $sumColumn;
+  }
+
+  return $sum;
 }
 
+print_r(
+  sumColumn([
+    [10, 10, 3],
+    [4, 5, 9],
+    [8, 5, 18],
+  ], 3)
+);
