861e01965aa90082229d22ca845d064802d915e7
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-11-19 21:09:14 +0300

    feat: One-dimensional_arrays 16

--- Two-dimensional_arrays/16/index.php
+++ Two-dimensional_arrays/16/index.php
@@ -4,9 +4,31 @@
  *  значений максимальных элементов строк
  */
 
-function f(array $arr ) 
+/** @var array[] $arr */
+function trasformMatrix(array $arr): array
 {
-  
-  return 
+  $l = count($arr);
+
+  for ($i = 0; $i < $l; $i++) {
+    for ($j = 1; $j < $l - $i; $j++) {
+      if (max($arr[$j]) < max($arr[$j - 1])) {
+        [$arr[$j], $arr[$j - 1]] = [$arr[$j - 1], $arr[$j]];
+      }
+    }
+  }
+
+  return $arr;
 }
 
+echo '<pre>';
+print_r(
+  trasformMatrix([
+    [101, 2, 100],
+    [1, 2, 70],
+    [1, 400, 3],
+    [1, 2, 100],
+    [1, 50, 1]
+
+  ])
+);
+echo '</pre>';
